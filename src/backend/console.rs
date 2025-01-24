059f29ca3972c95b4df0bedef2439663a62dc022
Marijan Šuflaj <msufflaj32@gmail.com>
2025-01-24 23:09:22 +0100

    Provide logger into backend
    Every backend now has same interface to
    log whatever it wants.

--- src/backend/console.rs
+++ src/backend/console.rs
@@ -1,4 +1,4 @@
-use crate::backend::Backend;
+use crate::backend::{Backend, Logger};
 use crate::metrics::TimeFrame;
 use chrono::{DateTime, Utc};
 
@@ -6,45 +6,49 @@ use chrono::{DateTime, Utc};
 pub struct Console {}
 
 impl Backend for Console {
-    fn publish(&mut self, time: &DateTime<Utc>, time_frame: &TimeFrame) {
-        println!("{} - host: {}", time.to_rfc3339(), time_frame.host);
+    fn publish(&mut self, time: &DateTime<Utc>, time_frame: &TimeFrame, logger: Logger) {
+        logger.info(&format!(
+            "{} - host: {}",
+            time.to_rfc3339(),
+            time_frame.host
+        ));
 
         if !time_frame.gauges.is_empty() {
-            println!("Gauges:");
+            logger.info("Gauges:");
 
             time_frame
                 .gauges
                 .iter()
-                .for_each(|(name, value)| println!("  {name} - {value}"));
+                .for_each(|(name, value)| logger.info(&format!("  {name} - {value}")));
         }
 
         if !time_frame.counters.is_empty() {
-            println!("Counters:");
+            logger.info("Counters:");
 
             time_frame.counters.iter().for_each(|(name, stats)| {
-                println!("  {name}");
-                println!("    count: {}", stats.count());
-                println!("    sum: {}", stats.sum());
-                println!("    avg: {}", stats.average());
-                println!("    std: {}", stats.std());
-                println!("    median: {}", stats.median());
-                println!("    p75: {}", stats.percentile(0.75));
-                println!("    p90: {}", stats.percentile(0.90));
+                logger.info(&format!("  {name}"));
+                logger.info(&format!("    count: {}", stats.count()));
+                logger.info(&format!("    sum: {}", stats.sum()));
+                logger.info(&format!("    avg: {}", stats.average()));
+                logger.info(&format!("    std: {}", stats.std()));
+                logger.info(&format!("    median: {}", stats.median()));
+                logger.info(&format!("    p75: {}", stats.percentile(0.75)));
+                logger.info(&format!("    p90: {}", stats.percentile(0.90)));
             });
         }
 
         if !time_frame.timings.is_empty() {
-            println!("Timings:");
+            logger.info("Timings:");
 
             time_frame.timings.iter().for_each(|(name, stats)| {
-                println!("  {name}");
-                println!("    count: {}", stats.count());
-                println!("    sum: {}", stats.sum());
-                println!("    avg: {}", stats.average());
-                println!("    std: {}", stats.std());
-                println!("    median: {}", stats.median());
-                println!("    p75: {}", stats.percentile(0.75));
-                println!("    p90: {}", stats.percentile(0.90));
+                logger.info(&format!("  {name}"));
+                logger.info(&format!("    count: {}", stats.count()));
+                logger.info(&format!("    sum: {}", stats.sum()));
+                logger.info(&format!("    avg: {}", stats.average()));
+                logger.info(&format!("    std: {}", stats.std()));
+                logger.info(&format!("    median: {}", stats.median()));
+                logger.info(&format!("    p75: {}", stats.percentile(0.75)));
+                logger.info(&format!("    p90: {}", stats.percentile(0.90)));
             });
         }
     }
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -5,7 +5,25 @@ use crate::metrics::TimeFrame;
 use chrono::{DateTime, Utc};
 
 pub trait Backend {
-    fn publish(&mut self, time: &DateTime<Utc>, time_frame: &TimeFrame);
+    fn publish(&mut self, time: &DateTime<Utc>, time_frame: &TimeFrame, logger: Logger);
+}
+
+pub struct Logger {
+    backend: String,
+}
+
+impl Logger {
+    pub fn new(backend: String) -> Self {
+        Logger { backend }
+    }
+
+    pub fn debug(&self, msg: &str) {
+        log::debug!("[{}] {}", self.backend, msg);
+    }
+
+    pub fn info(&self, msg: &str) {
+        log::info!("[{}] {}", self.backend, msg);
+    }
 }
 
 pub use console::Console;
